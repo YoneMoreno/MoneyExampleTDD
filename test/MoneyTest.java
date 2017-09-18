1fb75ace6e808ab3c3ced9acbf8aa54907437c2e
YonePC <YonePC@YONE>
2017-09-18 16:29:59 +0100

    We have moved the common code from the class Dollar to the superclass Money. We made a second class Franc also a subclass. We moved the overriding equals subclasses' impkementation to the parent, Money pag 34.

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -9,9 +9,14 @@ public class MoneyTest {
     @Test
     public void testMultiplication_given5timess2_expected10_given5timess3_expecteed15() {
         Dollar five = new Dollar(5);
-
         assertEquals(new Dollar(10).amount, five.times(2).amount);
         assertEquals(new Dollar(15).amount, five.times(3).amount);
+
+    }    @Test
+    public void testFrancMultiplication_given5timess2_expected10_given5timess3_expecteed15() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10).amount, five.times(2).amount);
+        assertEquals(new Franc(15).amount, five.times(3).amount);
 
     }
 
@@ -19,13 +24,25 @@ public class MoneyTest {
         public void equalityTest(){
             assertTrue(new Dollar(5).equals(new Dollar(5)));
             assertFalse(new Dollar(5).equals(new Dollar(6)));
+            assertTrue(new Franc(5).equals(new Franc(5)));
+            assertFalse(new Franc(5).equals(new Franc(6)));
         }
 
 
 
+    class Money{
+        protected int amount;
 
-    private class Dollar {
-        private int amount;
+        @Override
+        public boolean equals(Object o) {
+            Money money = (Money) o;
+            return amount == money.amount;
+        }
+    }
+
+
+    private class Dollar extends Money {
+
 
         public Dollar(int number) {
             amount = number;
@@ -36,10 +53,21 @@ public class MoneyTest {
 
         }
 
-        @Override
-        public boolean equals(Object o) {
-            Dollar dollar = (Dollar) o;
-            return amount == dollar.amount;
+
+    }
+
+    private class Franc extends Money {
+
+
+        public Franc(int number) {
+            amount = number;
         }
+
+        public Franc times(int multiplier) {
+            return new Franc(amount * multiplier);
+
+        }
+
+
     }
 }
