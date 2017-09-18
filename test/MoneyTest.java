8837e32dbe5ab84b9f53d1b7eab790eccd13bc2d
YonePC <YonePC@YONE>
2017-09-18 16:57:11 +0100

    We have reconciled both subclasses' times() implementation moving it up to Money through an abstract declaration. Also we have decoupled test code from the existence of concrete subclasses by introducing factory methods. It means we can now call Money.dollar(amount) instead of call to new Dollar(amount).

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -8,49 +8,59 @@ import static org.junit.Assert.assertTrue;
 public class MoneyTest {
     @Test
     public void testMultiplication_given5timess2_expected10_given5timess3_expecteed15() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10).amount, five.times(2).amount);
-        assertEquals(new Dollar(15).amount, five.times(3).amount);
+        Money five = Money.dollar(5);
+        assertEquals( Money.dollar(10).amount, five.times(2).amount);
+        assertEquals(Money.dollar(15).amount, five.times(3).amount);
 
     }    @Test
     public void testFrancMultiplication_given5timess2_expected10_given5timess3_expecteed15() {
-        Franc five = new Franc(5);
-        assertEquals(new Franc(10).amount, five.times(2).amount);
-        assertEquals(new Franc(15).amount, five.times(3).amount);
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10).amount, five.times(2).amount);
+        assertEquals(Money.franc(15).amount, five.times(3).amount);
 
     }
 
         @Test
-        public void equalityTest(){
-            assertTrue(new Dollar(5).equals(new Dollar(5)));
-            assertFalse(new Dollar(5).equals(new Dollar(6)));
-            assertTrue(new Franc(5).equals(new Franc(5)));
-            assertFalse(new Franc(5).equals(new Franc(6)));
-            assertFalse(new Franc(5).equals(new Dollar(5)));
+    public void equalityTest(){
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
 
-        }
+    }
 
 
 //
-    class Money{
+abstract static class Money{
         protected int amount;
+
+        abstract Money times(int multiplier);
 
         @Override
         public boolean equals(Object o) {
             Money money = (Money) o;
             return amount == money.amount && getClass().equals(money.getClass());
         }
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
     }
 
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+}
 
-    private class Dollar extends Money {
+
+    private static class Dollar extends Money {
 
 
         public Dollar(int number) {
             amount = number;
         }
 
-        public Dollar times(int multiplier) {
+        public Money times(int multiplier) {
             return new Dollar(amount * multiplier);
 
         }
@@ -58,14 +68,14 @@ public class MoneyTest {
 
     }
 
-    private class Franc extends Money {
+    private static class Franc extends Money {
 
 
         public Franc(int number) {
             amount = number;
         }
 
-        public Franc times(int multiplier) {
+        public Money times(int multiplier) {
             return new Franc(amount * multiplier);
 
         }
