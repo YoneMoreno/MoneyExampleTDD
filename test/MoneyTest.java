5cadafe5777686859174814c85534a0c9e4ec70b
YonePC <YonePC@YONE>
2017-09-18 09:46:14 +0100

    Triangulation and using Value Object pattern to test equality and inequality and implement equals()

--- test/MoneyTest.java
+++ test/MoneyTest.java
@@ -1,16 +1,30 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class MoneyTest {
     @Test
-    public void testMultiplication_given5times2_expected10_given5times3_expecteed15(){
+    public void testMultiplication_given5times2_expected10_given5times3_expecteed15() {
         Dollar five = new Dollar(5);
 
         Dollar product = five.time(2);
-        Assert.assertEquals(10, product.amount);
+        assertEquals(10, product.amount);
         product = five.time(3);
-        Assert.assertEquals(15, product.amount);
+        assertEquals(15, product.amount);
+
     }
+
+        @Test
+        public void equalityTest(){
+            assertTrue(new Dollar(5).equals(new Dollar(5)));
+            assertFalse(new Dollar(5).equals(new Dollar(6)));
+        }
+
+
+
 
     private class Dollar {
         public int amount;
@@ -22,6 +36,12 @@ public class MoneyTest {
         public Dollar time(int multiplier) {
             return new Dollar(amount * multiplier);
 
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            Dollar dollar = (Dollar) o;
+            return amount == dollar.amount;
         }
     }
 }
